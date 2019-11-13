cb1cf74eec19b3bb2a2091af22e64bb3ac265034
RodrigoAGM <rod-gm@hotmail.com>
2019-11-13 17:14:06 -0500

    User model created

---
+++ lib/model/customer.dart
@@ -0,0 +1,33 @@
+class Customer {
+  final int id;
+  final String name;
+  final String email;
+  final String password;
+  final String phone;
+  final int age;
+  final String image;
+
+  Customer({this.id, this.name, this.email, this.password, this.phone, this.age, this.image});
+
+  factory Customer.fromJson(Map<String, dynamic> json){
+    return Customer(
+      id : json['_id'],
+      name: json['full_name'],
+      email: json['email'],
+      password: json['password'],
+      phone: json['phone'],
+      age: json['age'],
+      image: json['image']
+    );
+  }
+
+  Map<String, dynamic> toJson() =>{
+    'id': id,
+    'name': name,
+    'email': email,
+    'password':password,
+    'phone': phone,
+    'age': age,
+    'image': image
+  };
+}
